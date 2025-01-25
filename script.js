2158fbdc151cec4acfcd7b7e9c1fd7f816e8d330
Marcus <marcusfonseca15@gmail.com>
2025-01-25 16:10:43 -0300

    switcher temrinado

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 //‘use strict’ //reduz erros silenciosos, melhora o desempenho,
             // fornece mais avisos e menos recursos não seguros
 
+document.addEventListener('DOMContentLoaded', function() {
 const trocar = document.querySelector('.btn');
 
 trocar.addEventListener('click', function(){
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('modo-dark');
+    document.body.classList.toggle('modo-claro');
 
     var nomeClasse = document.body.className;
 
     if(nomeClasse == "modo-claro"){
-        this.textContent = "Dark";
+        this.textContent = "Mudar para DARK";
     } 
     else {
-        this.textContent = "Claro";
-    }
+        this.textContent = "Mudar para CLARO";
+    } 
 
     console.log('Estado atual: ' + nomeClasse);
+    });
+
+    document.body.classList.add('modo-claro');
+    trocar.textContent = "Ativar para modo Escuro";
 });
 
